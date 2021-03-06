ce98035bb8b6521e28638c653d31b51ae2deba29
editorai <77742839+editorai@users.noreply.github.com>
2021-03-06 21:16:04 +0800

    Delete deploy2oss.sh

--- scripts/deploy2oss.sh
+++
@@ -1,8 +0,0 @@
-rm -rf dist
-npm run build
-
-~/ossutil config -e oss-cn-shanghai.aliyuncs.com -i LTAI4FramnvGWP9pHCKQtLUe -k 1XwgwGE7ovV5dGgiVvEDxRiDCsz7VN
-
-~/ossutil cp ./dist oss://pandachain-webpage/ --recursive --force --exclude ".DS_Store"
-
-echo "oss-pandachina.macroshen.cn"
